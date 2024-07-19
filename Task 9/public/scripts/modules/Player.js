df6c4fbcddb55141fde4315a0c5d35de40d678a3
deividas-butkus <deividas.butkus@hotmail.com>
2024-07-19 19:30:44 +0300

    task 9 - Player klasė pašalinta

--- Task 9/public/scripts/modules/Player.js
+++ Task 9/public/scripts/modules/Player.js
@@ -1,14 +1,1 @@
-export default class Player {
-    constructor(player) {
-        this.player = player;
-    }
-    render() {
-        const playerCard = document.createElement("div");
-        playerCard.classList.add("playerCard");
-        const fullNameTtl = document.createElement("h3");
-        fullNameTtl.textContent =
-            this.player.firstName + " " + this.player.lastName;
-        playerCard.append(fullNameTtl);
-        return playerCard;
-    }
-}
+"use strict";
