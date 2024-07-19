7e967773168be58fbeaf7c5f615530fbf5b4f22c
deividas-butkus <deividas.butkus@hotmail.com>
2024-07-19 08:36:52 +0300

    task 1 padarytas

--- Task 1/script.js
+++ Task 1/script.js
@@ -2,25 +2,24 @@
 Priskirkite tipus VISIEMS pateiktiems kintamiesiems, kad kodas veiktų teisingai.
 Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.
 -------------------------------------------------------------------------- */
-
-let one: number = 654;
-let two: boolean = true;
-let three: boolean = true;
-let four: boolean | string = true;
+var one = 654;
+var two = true;
+var three = true;
+var four = true;
 four = "123";
-let five: number[] | string[] | boolean[] = [1, 2, 3];
+var five = [1, 2, 3];
 five = [21, 22, 23, 24];
 five = ["a", "b"];
 five = [true, true, false];
-let six: (number | string | boolean)[] = [1, 2, 3, 4];
+var six = [1, 2, 3, 4];
 six = ["a", "b", "c", "d"];
 six = ["a", 2, 3, "d"];
 six = [true, false];
-let seven: (number | string | boolean)[] = [1, 2, 3, 4];
+var seven = [1, 2, 3, 4];
 seven = ["a", "b", "c", "d"];
 seven = ["a", 2, 3, "d"];
 seven = [true, false];
-let eight: (number | string | boolean)[] = ["hi", 1];
+var eight = ["hi", 1];
 eight = ["bye", 2];
 eight = [1, 2];
 eight = ["a", "b", "c"];
