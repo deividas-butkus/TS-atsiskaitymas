5555f6b71de6adfee5255ac0782c3cf68e447618
deividas-butkus <deividas.butkus@hotmail.com>
2024-07-19 11:29:59 +0300

    task 6 padarytas

--- Task 6/script.ts
+++ Task 6/script.ts
@@ -14,3 +14,33 @@ Pvz.:
   new_potion.color  =  [127, 159, 127]
   new_potion.volume =  19
 ------------------------------------------------------------------------------------------------------ */
+
+type PotionT = [number, number, number];
+
+class Potion {
+  color: PotionT;
+  volume: number;
+
+  constructor(color: PotionT, volume: number) {
+    this.color = color;
+    this.volume = volume;
+  }
+  mix(potion: Potion): Potion {
+    const newVolume = this.volume + potion.volume;
+    const newColor: PotionT = [
+      Math.ceil(
+        (this.color[0] * this.volume + potion.color[0] * potion.volume) /
+          newVolume
+      ),
+      Math.ceil(
+        (this.color[1] * this.volume + potion.color[1] * potion.volume) /
+          newVolume
+      ),
+      Math.ceil(
+        (this.color[2] * this.volume + potion.color[2] * potion.volume) /
+          newVolume
+      ),
+    ];
+    return new Potion(newColor, newVolume);
+  }
+}
