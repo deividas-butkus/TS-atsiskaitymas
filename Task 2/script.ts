b855e60ddd5bf2dabec4f4b170d8122fe5bc4fef
deividas-butkus <deividas.butkus@hotmail.com>
2024-07-19 08:53:51 +0300

    task 2 padarytas - su užkomentuotu three objektu

--- Task 2/script.ts
+++ Task 2/script.ts
@@ -2,43 +2,51 @@
 Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir visas kodas veiktų teisingai.
 Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
 type One = {
-
-}
+  vardas: string;
+  pavarde: string;
+  amzius: number;
+};
 let one: One = {
-  vardas: '',
-  pavarde: '',
-  amzius: 0
+  vardas: "",
+  pavarde: "",
+  amzius: 0,
 };
 type Two = {
-
-}
+  vardas: string;
+  pavarde: string;
+  amzius: number;
+  ugis?: number;
+  svoris?: number;
+};
 let two: Two = {
-  vardas: '',
-  pavarde: '',
-  amzius: 0
+  vardas: "",
+  pavarde: "",
+  amzius: 0,
 };
 two = {
-  vardas: '',
-  pavarde: '',
+  vardas: "",
+  pavarde: "",
   amzius: 0,
   ugis: 0,
-  svoris: 0
+  svoris: 0,
 };
 type Three = {
-
-}
+  vardas: string;
+  pavarde: string;
+  lytis: "vyras" | "moteris";
+};
 let three: Three = {
-  vardas: '',
-  pavarde: '',
-  lytis: 'vyras'
+  vardas: "",
+  pavarde: "",
+  lytis: "vyras",
 };
 three = {
-  vardas: '',
-  pavarde: '',
-  lytis: 'moteris'
+  vardas: "",
+  pavarde: "",
+  lytis: "moteris",
 };
-three = {
-  vardas: '',
-  pavarde: '',
-  lytis: 'sraigtasparnis' // error
-};
+// three = {
+//   vardas: "",
+//   pavarde: "",
+//   // lytis: "sraigtasparnis", // error
+// };
