e15d27391cf22ca78ce25809fe09ee1938e5d1f7
deividas-butkus <deividas.butkus@hotmail.com>
2024-07-19 10:53:42 +0300

    task 5 padarytas

--- Task 5/script.js
+++ Task 5/script.js
@@ -7,20 +7,16 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
-
-type Bet = "scissors" | "paper" | "rock";
-
-const checkWinner = (p1Bet: Bet, p2Bet: Bet): string => {
-  if (p1Bet === p2Bet) {
-    return "Draw!";
-  }
-  if (
-    (p1Bet === "rock" && p2Bet === "scissors") ||
-    (p1Bet === "scissors" && p2Bet === "paper") ||
-    (p1Bet === "paper" && p2Bet === "rock")
-  ) {
-    return "Player 1 won!";
-  } else {
-    return "Player 2 won!";
-  }
+var checkWinner = function (p1Bet, p2Bet) {
+    if (p1Bet === p2Bet) {
+        return "Draw!";
+    }
+    if ((p1Bet === "rock" && p2Bet === "scissors") ||
+        (p1Bet === "scissors" && p2Bet === "paper") ||
+        (p1Bet === "paper" && p2Bet === "rock")) {
+        return "Player 1 won!";
+    }
+    else {
+        return "Player 2 won!";
+    }
 };
